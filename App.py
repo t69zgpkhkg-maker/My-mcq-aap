a1987df278bcc81b2a07d018a2c61d07357b52c1
t69zgpkhkg-maker <t69zgpkhkg@privaterelay.appleid.com>
2026-07-15 01:26:27 +0530

    Update App.py

--- App.py
+++ App.py
@@ -27,8 +27,10 @@ if uploaded_file is not None:
                     prompt = f"Is image ke text ko achhe se samjho aur isse {num_questions} Multiple Choice Questions (MCQs) banao. Har question ke 4 options (A, B, C, D) hone chahiye aur end me Correct Answer aur chota sa explanation hona chahiye. Language Hinglish (Hindi + English mix) rakhein."
                     
                     response = client.models.generate_content(
-                        model='gemini-1.5-flash',
+                        model='gemini-1.5-flash-002',
                         contents=[image, prompt]
+)
+            
                     )
                     st.success("🎉 Aapke MCQs Taiyaar Hain!")
                     st.markdown(response.text)
